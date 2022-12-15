9c08fe86cff26be3c10419fa8ec57f0cd9e0a3b6
skolosov <skolosov.work@gmail.com>
2022-12-15 15:52:25 +0300

    drop unused controller

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -210,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\DocController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/docs', [DocController::class, 'getDocs']);
-Route::patch('/docs/{id}', [DocController::class, 'updateStatus'])->name('updateStatus');
 //Route::post('/docs', [DocController::class, 'store']);
 //Route::put('/docs/{id}', [DocController::class, 'update']);
 //Route::delete('/docs/{id}', [DocController::class, 'destroy']);
